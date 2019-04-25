dd437baa9b5411917f458150fdef9dffcc5e538b
AugustoChies <guto_chies@hotmail.com>
2019-04-25 11:53:26 -0300

    changed graph rule

--- graph.js
+++ graph.js
@@ -4,13 +4,8 @@
           
           function CreateGraph(points,hullPath)
           {
-             
-              
-              // var node = {x:0 , y: 0, edgelist:[]};
-              // var edge = {index1:0, index2:0, cost: 0};              
-              
               //push hullpath into nodes and create edges
-              for(var i= 0;i < hullPath.length;i++)
+              for(var i= 0;i < hullPath.length;i++) 
                   {
                       var index = findpointindex(hullPath[i],points);
                       points.splice(index,1);  
@@ -47,71 +42,84 @@
               var maiorMenorDistancia = 0;
               
               
-              
+              console.log(nodes.length);
               for(var i= 0;i < nodes.length;i++)
                   {
                       var menorDistancia = Infinity;                      
                       for(var j= i+1;j < nodes.length;j++)
                           {
-                              var tempdist = getDistance(nodes[i],nodes[j]);
+                              var tempdist = getDistance(nodes[i],nodes[j]);							 
                               if(tempdist < menorDistancia)
-                                  {
+                                  {									  
                                       menorDistancia = tempdist;                                      
                                   }
-                          }
-                      
-                      if(menorDistancia > maiorMenorDistancia)
+                          }                      
+					  
+                      if(menorDistancia > maiorMenorDistancia && menorDistancia != Infinity)
                           {
                               maiorMenorDistancia = menorDistancia;                              
                           }                      
                   }
               
-             // console.log(maiorMenorDistancia);
-            //  console.log("Hullpoint: x: " + hullPath[jhull].x + " y: " + hullPath[jhull].y);              
-              //console.log("Insidepoint: x: " + points[ipoint].x + " y: " + points[ipoint].y);
-              
               //creates aditional edges
-             
-              //one new edge per point for all points(none if no close point remains)
-              for(var i= 0;i < nodes.length;i++)
-                  {     
-                      var closesthullpoint = -2;//no point
-                      var edgeammount = Infinity;
-                      for(var j= i + 1;j < nodes.length;j++)
-                          {                              
-                              var tempdist = getDistance(nodes[i],nodes[j]);
-                              var tempedgecount = nodes[j].edgelist.length;
-                              if(tempdist <= maiorMenorDistancia)
-                                  {
-                                     var isdifferent = true;
-                                     for(var k = 0;k < nodes[i].edgelist.length;k++)
-                                         {
-                                            if(j == nodes[i].edgelist[k].index1 || j == nodes[i].edgelist[k].index2)
-                                                {
-                                                    isdifferent=false;
-                                                }
-                                         }
-                                      
-                                     if(tempedgecount < edgeammount && isdifferent)
-                                         {
-                                             edgeammount = tempedgecount;
-                                             closesthullpoint = j;
-                                         }
-                                      
-                                  }
-                          }
-                      if(closesthullpoint != -2)
+               //new edge for everyy point within distance
+			   for(var i= 0;i < nodes.length;i++)
+                  {                      
+                      for(var j= i+1;j < nodes.length;j++)
                           {
-                              var edge = {index1:closesthullpoint, index2:i, cost: getDistance(nodes[i],nodes[closesthullpoint])};         
-                              edgeList.push(edge);
-                              
-                              nodes[closesthullpoint].edgelist.push(edge);
-                              nodes[closesthullpoint].edgeindexlist.push(edgeList.length - 1);
-                              nodes[i].edgelist.push(edge);
-                              nodes[i].edgeindexlist.push(edgeList.length - 1);                              
-                          }
-                                   
-                  }             
+                              var tempdist = getDistance(nodes[i],nodes[j]);							 
+                              if(tempdist <= maiorMenorDistancia)
+                                  {									  
+                     	              var edge = {index1:j, index2:i, cost: 									getDistance(nodes[i],nodes[j])};         
+									  edgeList.push(edge);
+                     	        			
+									  nodes[j].edgelist.push(edge);
+                     	        	  nodes[j].edgeindexlist.push(edgeList.length - 1);
+                     	        	  nodes[i].edgelist.push(edge);
+                     	       		  nodes[i].edgeindexlist.push(edgeList.length - 1);                                     
+                                  }
+                          }           
+                  }
+              //one new edge per point for all points(none if no close point remains)
+              //for(var i= 0;i < nodes.length;i++)
+              //    {     
+              //        var closesthullpoint = -2;//no point
+              //        var edgeammount = Infinity;
+              //        for(var j= i + 1;j < nodes.length;j++)
+              //            {                              
+              //                var tempdist = getDistance(nodes[i],nodes[j]);
+              //                var tempedgecount = nodes[j].edgelist.length;
+              //                if(tempdist <= maiorMenorDistancia)
+              //                    {
+              //                       var isdifferent = true;
+              //                       for(var k = 0;k < nodes[i].edgelist.length;k++)
+              //                           {
+              //                              if(j == nodes[i].edgelist[k].index1 || j == nodes[i].edgelist[k].index2)
+              //                                  {
+              //                                      isdifferent=false;
+              //                                  }
+              //                           }
+              //                        
+              //                       if(tempedgecount < edgeammount && isdifferent)
+              //                           {
+              //                               edgeammount = tempedgecount;
+              //                               closesthullpoint = j;
+              //                           }
+              //                        
+              //                    }
+              //            }
+              //        if(closesthullpoint != -2)
+              //            {
+              //                var edge = {index1:closesthullpoint, index2:i, cost: getDistance(nodes[i],nodes[closesthullpoint])};         
+              //                edgeList.push(edge);
+              //                
+              //                nodes[closesthullpoint].edgelist.push(edge);
+              //                nodes[closesthullpoint].edgeindexlist.push(edgeList.length - 1);
+              //                nodes[i].edgelist.push(edge);
+              //                nodes[i].edgeindexlist.push(edgeList.length - 1);                              
+              //            }
+              //                     
+              //    }             
 		  }
           
           function megaDebug()
